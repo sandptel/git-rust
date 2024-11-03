bffa01bf4101c6ffc2f6b538fd22b7340222d6bc
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-11-03 07:51:05 +0530

    first commit

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,45 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+// use clap::Command;
+use clap::{Parser,Subcommand};
+
+#[derive(Parser,Debug)]
+#[command(version,about,long_about=None)]
+struct Args{
+#[command(subcommand)]
+command:Command,
+
+}
+
+#[derive(Debug,Subcommand)]
+enum Command{
+    Init,
+}
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
+    // let args: Vec<String> = env::args().collect();
+    let args= Args::parse();
+
+    match args.command{
+        Command::Init=>{
+            fs::create_dir(".git").unwrap();
         fs::create_dir(".git/objects").unwrap();
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+        }
+        _=>{
+            
+        }
     }
+
+
+   
+        
 }
